441455fb059fb946bdc9997cedffd14027101325
luoshixing <416010171@qq.com>
2021-06-06 11:30:52 +0800

    初始化架构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,67 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider :trigger="null" collapsible v-model="collapsed">
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline">
+        <a-sub-menu key="1">
+          <span slot="title">
+            <a-icon type="user" />
+            <span>Vue</span>
+          </span>
+          <a-menu-item key="1-1">
+            <a href="/vue#/"> Home </a>
+          </a-menu-item>
+          <a-menu-item key="1-2">
+            <a href="/vue#/about"> About </a>
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-content
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px',
+        }"
+      >
+        <div class="content">
+          <!--这是右侧内容栏-->
+          <div id="single-vue" class="single-spa-vue">
+            <div id="vue"></div>
+          </div>
+        </div>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
-
+<script>
+export default {
+  data () {
+    return {
+      collapsed: false,
+    };
+  }
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
 }
 
-#nav {
-  padding: 30px;
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
 }
 </style>
